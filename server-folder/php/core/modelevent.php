f9b2c98cd11658dce044ff3d3851dd93af530280
Simon Roesler <simon@simon-VirtualBox.(none)>
2013-05-29 13:22:12 +0200

    as

---
+++ server-folder/php/core/modelevent.php
@@ -0,0 +1,35 @@
+<?php
+trait ModelEvent
+{
+	protected $events = array('on' => array());
+	
+	
+	public function on($name, $callback)
+	{
+		if(is_callable($callback))
+			$this->events['on'][$name][] = $callback;
+	}
+	
+	public function fire($name /*, ... */)
+	{
+		$args = func_get_args();
+		unset($args[0]);
+		
+		if(isset($this->events['on'][$name]))
+		{
+			foreach($this->events['on'][$name] as $callback)
+				call_user_func_array($callback, $args);			
+		}
+	}
+
+	public function cancel($name)
+	{
+		if(isset($this->events['on'][$name]))
+			unset($this->events['on'][$name]);
+	}
+
+	public function cancelAll()
+	{
+		$this->events['on'] = array();
+	}
+}
