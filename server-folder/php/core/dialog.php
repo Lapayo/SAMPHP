92cd6089dd25922664a9f68634dc8dc67a0bb174
habecker <yannick.habecker@googlemail.com>
2013-06-03 16:13:32 +0200

    Update dialog.php

--- server-folder/php/core/dialog.php
+++ server-folder/php/core/dialog.php
@@ -1,10 +1,14 @@
 <?php
 class Dialog
 {
-  use ModelEvent;
+	use ModelEvent;
+
+	protected static $minDialogId = 30000;
+	protected static $maxDialogId = 32767;
 
 	protected static $instances = array();
 	protected $id;
+	protected $player; //Player who is currently using the Dialog
 	protected $style;
 	public $caption;
 	public $info;
@@ -17,11 +21,16 @@ class Dialog
 
 	public static function find($id)
 	{
-		foreach(static::$instances as $key => $instance)
-		{
+		foreach(static::$instances as $instance)
 			if($instance->id === $id)
-				return static::$instances[$key];
-		}
+				return $instance;
+	}
+
+	public static function findForPlayer($player)
+	{
+		foreach(static::$instances as $instance)
+			if($instances->player === $player)
+				return $instance;
 	}
 
 	protected function __construct($style, $caption, $button1, $button2)
@@ -36,7 +45,6 @@ class Dialog
 	{
 		if($this->style == DIALOG_STYLE_LIST)
 			$this->info[] = array('item' => $itemtext, 'value' => $value);
-
 		return $this;
 	}
 
@@ -56,40 +64,54 @@ class Dialog
 	public function showPlayer($player)
 	{
 		$info = $this->info;
-		
+		for($id = static::$minDialogId; static::find($id) instanceof Dialog; $id++);
+
+		if($id >= static::$maxDialogId)
+		{
+			echo "Max dialogid: ".static::$maxDialogId." reached!";
+			return false;
+		}
+		$this->id = $id;
+		$this->player = $player;
 		if($this->style == DIALOG_STYLE_LIST)
 		{
 			unset($info);
 
 			foreach($this->info as $item)
-			{
 				$info[] = $item['item'];
-			}
-			$info = implode('\n', $info);
+
+			$info = implode("\n", $info);
 		}
-
-
 		ShowPlayerDialog($player->id, $this->id, $this->style, $this->caption, $info, $this->button[1], $this->button[0]);
-
 		return $this;
+	}
+
+	private function setIdToNull()
+	{
+		$this->id = null;
+		$this->player = null;
+	}
+
+	public static function handleUnexpectedClosing($player)
+	{
+		if(($dialog=static::findForPlayer($player)) instanceof Dialog)
+			$dialog->setIdToNull();
 	}
 
 	public static function handleResponse($player, $dialogid, $response, $listitem, $inputtext)
 	{
-		$dialog = Dialog::find($dialogid);
-
-		echo 'Dialog: '.$dialog->id.' showed to Player: '.$player->id;
-		echo 'Selected listitem = '.$listitem;
-		echo 'Selected button = '.$response;
-
+		$dialog = static::find($dialogid);
 		if($dialog->style == DIALOG_STYLE_LIST)
-			$listvalue = $dialog->info[$listitem];
+			$value = $listitem;
+			if($dialog->info[$listitem]['value'] !== null)
+				$value = $dialog->info[$listitem]['value'];
 		else
-			$listvalue = $listitem = null;
-
-		Event::fire('Response', $player, $dialog, $inputtext, $dialog->button[$response], $listitem, $listvalue);
+			$value = $listitem = null;
+		$dialog->setIdToNull();
+		$dialog->fire('Response', $player, $dialog, $inputtext, $dialog->button[$response], $value);
 	}
 
 }
 
 Event::on('DialogResponse', array('Dialog', 'handleResponse'));
+Event::on('PlayerDisconnect',array('Dialog','handleUnexpectedClosing'));
