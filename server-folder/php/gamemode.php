3da1bd3d122a5bbe8493938024250beca937333f
Simon Roesler <simon@sney.net>
2013-06-07 03:12:18 +0200

    some debugging functions

--- server-folder/php/gamemode.php
+++ server-folder/php/gamemode.php
@@ -1,3 +1,11 @@
 <?php
 require 'core/bootstrap.php';
 require 'grandlarc/gamemode.php';
+
+CommandText::register('/memory', function($player) {
+	$player->sendClientMessage(0xFFFFFFFF, "Memory Usage PHP: ".(memory_get_usage(false) / 1024)."KB (".(memory_get_usage(true) / 1024)."KB)");
+});
+
+CommandText::register('/garbage', function($player) {
+	$player->sendClientMessage(0xFFFFFFFF, "Collected ".gc_collect_cycles()." cycles");
+});
