12d7f2f7eda51a6b9c8c1e0899b33a97e385bb8d
Simon Roesler <simon@sney.net>
2013-06-12 18:23:27 +0200

    build fix

--- debug.sh
+++ debug.sh
@@ -1,8 +1,6 @@
 ./build.sh
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-BUILDDIR=$DIR/build
 
 #debug helper: Move directly to samp server
-cp $BUILDDIR/samphp $DIR/server-folder/plugins/samphp
+cp ./samphp ./server-folder/plugins/samphp
 
