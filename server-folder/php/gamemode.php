b70ca48da266068a24be8c140a0838cc15a3da66
Carlos Rodrigues <kikito.rodrigues@gmail.com>
2014-11-11 22:20:58 +0000

    Update
    Change the class name from the deprecated one(and no longer active) CommandText, for Command & CMD, to show both ways works.

--- server-folder/php/gamemode.php
+++ server-folder/php/gamemode.php
@@ -3,14 +3,14 @@ require 'core/bootstrap.php';
 
 require 'grandlarc/gamemode.php';
 
-CommandText::register('/memory', function($player) {
+Command::Register('/memory', function($player) {
 	$player->sendClientMessage(0xFFFFFFFF, "Memory Usage PHP: ".(memory_get_usage(false) / 1024)."KB (".(memory_get_usage(true) / 1024)."KB)");
 });
 
-CommandText::register('/garbage', function($player) {
+Command::register('/garbage', function($player) {
 	$player->sendClientMessage(0xFFFFFFFF, "Collected ".gc_collect_cycles()." cycles");
 });
 
-CommandText::register('/rr', function($player) {
+CMD::register('/rr', function($player) {
 	GameModeExit();
 });
